318bd3dd87d18e2fd3eb9769f569612f9b04f895
DittrichLucas <ld.dittrich@gmail.com>
2018-10-23 11:47:16 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: "./app/App.js",
+    output: {
+      filename: "public/bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            query: {
+                presets: ['@babel/react']
+              }
+          },
+        }
+      ]
+    }
+  }
